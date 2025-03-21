4129781fa0bb2689aa74a403ee3e5ee28469111e
bipashabanerjee628 <bbipasha305@gmail.com>
2025-03-21 19:31:21 +0530

    Add files via upload
    Snake Water Gun Game 🐍💧🔫
    A simple Python-based command-line game where the player competes against the computer in a classic "Snake, Water, Gun" game, similar to Rock-Paper-Scissors. The game uses random choices for the computer and keeps track of the score.
    
    Features:
    ✅ User vs Computer gameplay
    ✅ Randomized computer choices
    ✅ Simple and interactive CLI interface

---
+++ snake_water_game.py
@@ -0,0 +1,39 @@
+# PROJECT 1 ---------- SNAKE WATER GUN GAME ----------------------
+
+'''
+1 for snake
+-1 for water
+0 for gun
+
+'''
+import random
+computer = random.choice ([1,-1,0])
+youstr =  (input("Enter your choice: "))
+youDict = {"s" :1, "w": -1, "g": 0}
+reverseDict={1:"snake", -1:"water", 0:"gun"}
+
+you = youDict[youstr]
+
+print(f'computer chose {reverseDict[computer]}')
+print(f'you chose {reverseDict[you]}')
+
+if(computer == you):
+    print("It's a tie!")
+
+elif(computer ==-1 and you == 1):
+    print("you win!")
+elif(computer ==-1 and you == 0):
+    print("you lose!")
+elif(computer ==1 and you == -1):
+    print("you lose!")
+elif(computer ==1 and you == 0):
+    print("you win!")
+elif(computer ==0 and you == 1):
+    print("you lose!")
+elif(computer ==0 and you == -1):
+    print("you win!")
+else:
+    print("something went wrong")
+
+
+    print(" "*5+ "GAME OVER")
